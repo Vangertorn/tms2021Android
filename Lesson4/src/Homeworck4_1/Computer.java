caaf7e3003858b65308a4b3f6ae166bd04b304b3
vangertorn <vangertorn@yandex.ru>
2021-02-03 17:21:37 +0300

    I repaired my code and it's working correctly now

--- Lesson4/src/Homeworck4_1/Computer.java
+++ Lesson4/src/Homeworck4_1/Computer.java
@@ -9,51 +9,67 @@ public class Computer {
     private int drive;
     private int counter;
     private int condition = 1;
+    private int limitCounter;
 
     public Computer(String processor, int operational, int drive) {
+        Scanner a = new Scanner(System.in);
+        System.out.print("Enter limited counter\t");
+        this.limitCounter = a.nextInt();
         this.processor = processor;
         this.operational = operational;
         this.drive = drive;
-
     }
 
-
-    public void Info() {
-        System.out.println("processor\t" + processor + "\noperational\t" + operational + "\ndrive\t" + drive + "\ncounter\n" + counter);
+    public void info() {
+        System.out.println("processor\t" + processor + "\noperational\t" + operational + "\ndrive\t" + drive + "\ncounter\t" + counter);
     }
 
-    public void Enable() {
-        this.counter++;
-        if (this.condition == 0 || this.counter > 3) {
-            this.counter--;
+    public void enable() {
+        if (this.condition == 0 || this.counter >= this.limitCounter) {
             System.out.println("the computer burned down");
             return;
-
         }
-
         Random random = new Random();
         int b = random.nextInt(2);
+        //      It's string for wimps;
+//                System.out.println(b);
         Scanner scanner = new Scanner(System.in);
         System.out.println("You enable computer enter number 1 or 0");
         int a = scanner.nextInt();
         if (a == b) {
             this.condition = 1;
+            this.counter++;
         } else {
             this.condition = 0;
         }
+        if (this.condition == 0) {
 
+            System.out.println("the computer burned down");
+            return;
+        }
     }
 
-    public void Off() {
+    public void off() {
+        if (this.condition == 0 || this.counter >= this.limitCounter) {
+            System.out.println("the computer burned down");
+            return;
+        }
         Random random = new Random();
         int b = random.nextInt(2);
+        //       It's string for wimps;
+//                System.out.println(b);
         Scanner scanner = new Scanner(System.in);
         System.out.println("You turn off computer enter number 1 or 0");
         int a = scanner.nextInt();
         if (a == b) {
             this.condition = 1;
+            this.counter++;
         } else {
             this.condition = 0;
+        }
+        if (this.condition == 0) {
+            System.out.println("the computer burned down");
+            return;
         }
 
     }
--- Lesson4/src/Homeworck4_1/Menu.java
+++ Lesson4/src/Homeworck4_1/Menu.java
@@ -1,14 +1,17 @@
 package Homeworck4_1;
 
+import java.util.Scanner;
+
 public class Menu {
     public static void main(String[] args) {
         Computer computer1 = new Computer("pent", 256, 10);
-
-        computer1.Enable();
-        computer1.Off();
-        computer1.Enable();
-        computer1.Info();
-
-
+        System.out.print("How many time enable and off computer?\t");
+        Scanner a = new Scanner(System.in);
+        int b = a.nextInt();
+        for (int i = 0; i < b; i++) {
+            computer1.enable();
+            computer1.off();
+        }
+        computer1.info();
     }
 }
