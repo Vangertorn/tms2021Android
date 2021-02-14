ff067883599d26551b6e1112a09df83fc07e9050
vangertorn <vangertorn@yandex.ru>
2021-02-14 10:08:13 +0300

    I created 3 robots with different in parts. You can find among them the most expensive

---
+++ robot/robot/src/by/teachmeskills/robot/Robot.java
@@ -0,0 +1,73 @@
+package by.teachmeskills.robot;
+
+import by.teachmeskills.robot.hands.IHand;
+import by.teachmeskills.robot.heads.IHead;
+import by.teachmeskills.robot.legs.ILeg;
+
+public class Robot implements IRobot {
+    private IHead head;
+    private IHand hand;
+    private ILeg leg;
+    private String name;
+
+    public Robot(IHead head, IHand hand, ILeg leg, String name) {
+        this.head = head;
+        this.hand = hand;
+        this.leg = leg;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Robot(){}
+
+    public IHead getHead() {
+        return head;
+    }
+
+    public void setHead(IHead head) {
+        this.head = head;
+    }
+
+    public IHand getHand() {
+        return hand;
+    }
+
+    public void setHand(IHand hand) {
+        this.hand = hand;
+    }
+
+    public ILeg getLeg() {
+        return leg;
+    }
+
+    public void setLeg(ILeg leg) {
+        this.leg = leg;
+    }
+
+    @Override
+    public void action() {
+        head.speak();
+        hand.upHand();
+        leg.step();
+    }
+
+    /**
+     * Get the cost of a robot
+     * @return int
+     */
+    @Override
+    public int getPrice() {
+        int price = head.getPrice() + hand.getPrice() + leg.getPrice();
+        return price;
+    }
+
+
+
+}
---
+++ robot/robot/src/by/teachmeskills/robot/hands/SamsungHand.java
@@ -0,0 +1,20 @@
+package by.teachmeskills.robot.hands;
+
+public class SamsungHand implements IHand{
+    private int price;
+
+    public SamsungHand(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public void upHand() {
+        System.out.println("Up hand Samsung");
+
+    }
+
+    @Override
+    public int getPrice() {
+        return price;
+    }
+}
